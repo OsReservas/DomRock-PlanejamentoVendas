a6c0eeaee6e9a8295517552c289af4b90a1f109c
bea <beatrielopes@outlook.com>
2023-03-26 00:19:03 -0300

    criação de campos e sidebar - frontend

--- TelaApi/src/app/app-routing.module.ts
+++ TelaApi/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path:'',pathMatch:'full', redirectTo: 'historico'},
+  {
+    path:'',
+    pathMatch:'full', 
+    component: HomeComponent,
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
 
   {path:'historico',
   loadChildren: () => import('./historico/historico.module').then(m => m.HistoricoModule)}
--- TelaApi/src/app/app.module.ts
+++ TelaApi/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule, } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
