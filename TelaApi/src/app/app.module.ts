75872f7f4565b609049c42f308876891737a3273
bea <beatrielopes@outlook.com>
2023-03-26 15:12:25 -0300

    correcao sharedmodule

--- TelaApi/src/app/app.module.ts
+++ TelaApi/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    SharedModule,
     HttpClientModule
 
   ],
