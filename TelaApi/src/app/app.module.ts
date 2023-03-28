737433a729e7572e598b262518fdff95a2892e90
bea <beatrielopes@outlook.com>
2023-03-27 21:31:19 -0300

    arrumando campo produto

--- TelaApi/src/app/app.module.ts
+++ TelaApi/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -18,8 +23,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     MatToolbarModule,
     SharedModule,
-    HttpClientModule
-
+    HttpClientModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TelaApi/src/app/home/home.module.ts
+++ TelaApi/src/app/home/home.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -11,7 +13,11 @@ import { MatCardModule } from '@angular/material/card';
     CommonModule,
     ReactiveFormsModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    CommonModule,
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
   ],
   exports:[HomeComponent]
 })
