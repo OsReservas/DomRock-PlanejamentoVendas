f076756ccbb056a7c5bc64bcc2fd193ec3f40ac4
ThiagoChaves13 <thiagolopeschaves@hotmail.com>
2023-03-30 22:58:57 -0300

    atulização de configuração de erro

--- TelaApi/src/app/import/aplicacao/aplicacao.module.ts
+++ TelaApi/src/app/import/aplicacao/aplicacao.module.ts
@@ -13,9 +13,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
    MatCardModule,
    MatTableModule,
    MatToolbarModule,
-   AvatarModule,
-   ProgressBarModule,
-   ToastModule,
    MatProgressSpinnerModule
   ]
 })
