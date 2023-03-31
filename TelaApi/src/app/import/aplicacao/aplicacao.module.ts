6746d0dd8ccb2e9b469425837d821a425930989c
beatricelopes <beatricelopes@outlook.com>
2023-03-30 21:54:30 -0300

    mudancas no front (nao ta pronto)

--- TelaApi/src/app/import/aplicacao/aplicacao.module.ts
+++ TelaApi/src/app/import/aplicacao/aplicacao.module.ts
@@ -2,23 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AvatarModule } from 'primeng/avatar';
-import { ProgressBarModule } from 'primeng/progressbar';
 // For dynamic progressbar demo
-import { ToastModule } from 'primeng/toast';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-
-
 
 @NgModule({
   exports: [
    MatCardModule,
    MatTableModule,
    MatToolbarModule,
-   AvatarModule,
-   ProgressBarModule,
-   ToastModule,
    MatProgressSpinnerModule
   ]
 })
