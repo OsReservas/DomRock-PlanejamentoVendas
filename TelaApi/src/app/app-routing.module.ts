1bb4107936911a6f5725738fa7f51adfcb3df454
beatricelopes <beatricelopes@outlook.com>
2023-04-09 19:07:56 -0300

    login criado

--- TelaApi/src/app/app-routing.module.ts
+++ TelaApi/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroVendaComponent } from './cadastro-vendas/cadastro-vendas.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
+  { 
     path:'',
-    pathMatch:'full',
-    redirectTo: 'CadastroVendas'
+    component: LoginComponent,
   },
-  { path:'cadastroVendas',
+  { 
+    path:'cadastroVendas',
     component: CadastroVendaComponent,
     loadChildren: () => import('./cadastro-vendas/cadastro-vendas.module').then(m => m.CadastroVendaModule)
   },
