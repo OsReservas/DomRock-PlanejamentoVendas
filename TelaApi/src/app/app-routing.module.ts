af6d8212ca65cb0b3e3d7a19a08d8867d9d2c1d2
beatricelopes <beatricelopes@outlook.com>
2023-04-09 17:14:32 -0300

    up

--- TelaApi/src/app/app-routing.module.ts
+++ TelaApi/src/app/app-routing.module.ts
@@ -2,25 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroVendaComponent } from './cadastro-vendas/cadastro-vendas.component';
 
-
 const routes: Routes = [
   {
     path:'',
     pathMatch:'full',
     redirectTo: 'CadastroVendas'
   },
-
-  
   { path:'cadastroVendas',
     component: CadastroVendaComponent,
     loadChildren: () => import('./cadastro-vendas/cadastro-vendas.module').then(m => m.CadastroVendaModule)
   },
-
-  {path:'historico',
-  loadChildren: () => import('./historico/historico.module').then(m => m.HistoricoModule)},
-
-  {path:'meus-clientes',
-  loadChildren: () => import('./meus-clientes-b/meus-clientes-b.module').then(m => m.MeusClientesModule)}
+  {
+    path:'historico',
+    loadChildren: () => import('./historico/historico.module').then(m => m.HistoricoModule)
+  },
+  {
+    path:'meus-clientes',
+    loadChildren: () => import('./meus-clientes-b/meus-clientes-b.module').then(m => m.MeusClientesModule)
+  }
 ];
 
 @NgModule({
